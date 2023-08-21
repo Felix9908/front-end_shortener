1ac3fa5d14e22b32876fc4a009d30f933cb34492
Kevin Díaz Moya <kevin.moya1301@gmail.com>
2023-08-21 19:33:58 -0400

    Añadido skeleton cutre

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Card, Skeleton} from "@nextui-org/react";
 
 function Statistics() {
   return (
@@ -24,8 +25,11 @@ function Statistics() {
       </div>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-14 w-full bg-red-300">
         <div className="flex items-center flex-col bg-yellow-500 w-[250px] h-[150px] m-10">
-          <p className="text-6xl p-5">0,00</p>
-          <p>Vistas totales</p>
+                  <Card className="w-[200px] space-y-5 p-4" radius="2xl">
+                    <Skeleton className="rounded-lg">
+                        <div className="h-24 rounded-lg bg-default-300"></div>
+                    </Skeleton>
+                  </Card>
         </div>
         <div className=" flex items-center flex-col bg-green-500 w-[250px] h-[150px] m-10">
           <p className="text-6xl p-5">0,00</p>
