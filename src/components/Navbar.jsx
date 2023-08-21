c1a19eae4803908b5ddc11f1c7a6a19893d035a2
Kevin Díaz Moya <kevin.moya1301@gmail.com>
2023-08-21 17:41:52 -0400

    Retocado el menu desplegable de la foto de perfil

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,17 +43,16 @@ export default function App() {
           </DropdownTrigger>
           <DropdownMenu aria-label="Profile Actions" variant="flat">
             <DropdownItem key="profile" className="h-14 gap-2">
-              <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">zoey@example.com</p>
+              <p className="font-semibold">Bienvenido</p>
+              <p className="font-semibold">Jhon Doe</p>
             </DropdownItem>
-            <DropdownItem key="settings">My Settings</DropdownItem>
-            <DropdownItem key="team_settings">Team Settings</DropdownItem>
-            <DropdownItem key="analytics">Analytics</DropdownItem>
-            <DropdownItem key="system">System</DropdownItem>
-            <DropdownItem key="configurations">Configurations</DropdownItem>
-            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="settings">Configurar perfil</DropdownItem>
+            <DropdownItem key="team_settings">Mi equipo</DropdownItem>
+            <DropdownItem key="analytics">Mis Analíticas</DropdownItem>
+            <DropdownItem key="system">Analíticas Globales</DropdownItem>
+            <DropdownItem key="help_and_feedback">Ayuda</DropdownItem>
             <DropdownItem key="logout" color="danger">
-              Log Out
+              Cerrar sesión
             </DropdownItem>
           </DropdownMenu>
         </Dropdown>
