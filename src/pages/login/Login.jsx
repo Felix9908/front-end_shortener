676c5717171ecc5e88727f0cc1aa0ea52b5cc92c
Kevin Díaz Moya <kevin.moya1301@gmail.com>
2023-09-03 13:52:03 -0400

    Simple cambio en el login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
   };
 
   return (
-    <div>
+    <div className="w-full">
       <div className="flex items-center justify-center h-screen bg-gray-100">
         <div className="bg-white p-8 rounded shadow-md w-full sm:w-96">
           <h2 className="text-2xl font-semibold mb-4">Iniciar Sesión</h2>
