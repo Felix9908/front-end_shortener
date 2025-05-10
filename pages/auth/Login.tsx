3702be6de47c881cb1b829b24053fe538fec80a5
Kevin Diaz Moya <kevin.moya1301@gmail.com>
2025-05-09 18:39:46 -0600

    Cambio pequeño

--- pages/auth/Login.tsx
+++ pages/auth/Login.tsx
@@ -1,6 +1,21 @@
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login-page">
+        <div className="container">
+            <div className="wrapper">
+                <input type="text" placeholder="Username"/>
+            </div>
+
+            <div className="wrapper">
+                <input type="text" placeholder="asdasdas"/>
+            </div>
+
+            <div className="wrapper">
+                <input type="text" placeholder="asdasdas"/>
+            </div>
+            
+        </div>
+    </div>
   )
 }
 
