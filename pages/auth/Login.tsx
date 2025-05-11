cab7271fc9415c08af71c95cee3322727b576db0
Kevin Diaz Moya <kevin.moya1301@gmail.com>
2025-05-10 20:21:45 -0600

    Cambios en progreso

--- pages/auth/Login.tsx
+++ pages/auth/Login.tsx
@@ -1,10 +1,16 @@
+import LanguageSwitcher from "components/common/LanguageSwitcher"
 import { LoginPageVars } from "constants/ui/auth/LoginPageVars"
+import { useTranslation } from "react-i18next"
 
 const Login = () => {
+  const { t } = useTranslation("auth");
+
   return (
     <div className="login-page">
         <section className="left-secton">
           <img src={LoginPageVars.bgPath} alt="" />
+          <LanguageSwitcher/>
+          <h1>{t("loginPage.title")}</h1>
         </section>
 
         <section className="right-section">
