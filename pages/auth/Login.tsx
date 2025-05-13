bb208df169550f39cce615d2c84c01e1d19aef88
Kevin Diaz Moya <kevin.moya1301@gmail.com>
2025-05-13 17:39:02 -0600

    Cambios no responsives

--- pages/auth/Login.tsx
+++ pages/auth/Login.tsx
@@ -6,14 +6,19 @@ const Login = () => {
   const { t } = useTranslation("auth");
 
   return (
-    <div className="login-page overflow-hidden">
+    <div className="login-page">
         <section className="left-secton">
           <div className="absolute -left-10 rotate-y-180 lg:rotate-y-0">
             <img src={LoginPageVars.bgPath} alt=""/>
           </div>
           
-
+          <div className="absolute flex flex-col gap-2 lg:gap-5 bottom-35 left-15">
           <LanguageSwitcher/>
+
+            <h1 className="text-white font-bold text-5xl">{t("loginPage.welcomeMessage")}</h1>
+            <h1 className="bg-gradient-to-b from-[#501794] to-[#AE69FF] bg-clip-text text-transparent font-bold text-5xl">{t("loginPage.welcomeMessageAccent")}</h1>
+          </div>
+
           <h1>{t("loginPage.title")}</h1>
         </section>
 
@@ -21,7 +26,7 @@ const Login = () => {
 
         </section>
 
-        <div className="fade"/>
+        <div className="login-fade-bottom"/>
     </div>
   )
 }
